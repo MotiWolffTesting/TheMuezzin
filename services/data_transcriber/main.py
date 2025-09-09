93474a87bb9e88a2d546eb39ecd3e30e4d90f6d0
MotiWolff <motiwolff@gmail.com>
2025-09-09 11:25:16 +0300

    Implement initial Dockerfile and main application logic for data transcriber service

---
+++ services/data_transcriber/main.py
@@ -0,0 +1,23 @@
+from shared.logger import Logger
+from .config import DataTranscriberConfig
+from mongodb_transcriber import TranscriptionManager
+
+
+def main() -> int:
+    "Main function to run transcriber"
+    config = DataTranscriberConfig.from_env()
+    logger = Logger.get_logger(
+        name="data_transcriber_service",
+        es_host=config.logger_es_host,
+        index=config.logger_index,
+    )
+    logger.info("The Muezzin data transcriber service started successfully")
+
+    manager = TranscriptionManager(config=config)
+    manager.run()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+
